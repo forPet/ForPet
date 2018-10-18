9fdac7c06c235a1b30b84e120a3759b4d9d4fa7a
jhchon <jjh5999@naver.com>
2018-10-18 14:57:58 +0900

    DB 커넥터 테스트 및 DB 연결

--- ForPet/src/test/java/com/forpet/test/Test.java
+++ ForPet/src/test/java/com/forpet/test/Test.java
@@ -1,5 +1,27 @@
 package com.forpet.test;
 
-public class Test {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+public class Test {
+	private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+	
+	private static final String URL = "jdbc:mysql://192.168.50.37:3306/forpet?useSSL=false&serverTimezone=Asia/Seoul";
+	
+	private static final String USER = "forpet";
+	
+	private static final String PWD = "forpet";
+
+	@org.junit.Test
+	public void testConn() throws Exception {
+		Class.forName(DRIVER);
+		
+		try {
+			Connection con = DriverManager.getConnection(URL, USER, PWD);
+			System.out.println(con);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
+
