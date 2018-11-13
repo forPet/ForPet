a4b01345d83d0e41700aab46f09c13f52712092a
jhchon <jjh5999@naver.com>
2018-11-13 20:52:46 +0900

    테스트코드 작성 - 백업커밋

--- ForPet/src/test/java/com/forpet/test/PaymentTest.java
+++ ForPet/src/test/java/com/forpet/test/PaymentTest.java
@@ -9,6 +9,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.forpet.domain.BookingVO;
 import com.forpet.persistence.booking.PaymentDAO;
+import com.forpet.persistence.userPage.MyPageDAO;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/spring/**/root-context.xml" })
@@ -16,7 +17,10 @@ public class PaymentTest {
 	@Inject
 	private PaymentDAO dao;
 
-	// @Test
+	@Inject
+	private MyPageDAO dao2;
+	
+	//@Test
 	public void payment() throws Exception {
 		BookingVO vo = new BookingVO();
 		vo.setBookingNumber(1);
@@ -24,18 +28,23 @@ public class PaymentTest {
 		System.out.println(vo);
 	}
 
-	// @Test
+	//@Test
 	public void sitterSelect() throws Exception {
 		dao.sitterSelect(1);
 	}
 
-	@Test
+	//@Test
 	public void sitterServiceSel() throws Exception {
 		dao.sitterServiceSel(1);
 	}
 
-	@Test
+	//@Test
 	public void sitterExtra() throws Exception {
 		dao.sitterExtra(1);
+	}
+	
+	@Test
+	public void test() throws Exception {
+		dao2.reservationSearch(2);
 	}
 }
