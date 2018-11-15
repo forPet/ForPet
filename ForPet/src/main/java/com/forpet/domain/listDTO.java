ec6a34425ad61e47e562e2ed296ae215c68bf4b6
WinterMelong <ttool4189@gmail.com>
2018-11-15 09:30:21 +0900

    ListDTO 추가

---
+++ ForPet/src/main/java/com/forpet/domain/listDTO.java
@@ -0,0 +1,49 @@
+package com.forpet.domain;
+
+public class listDTO {
+	private String userName;
+	private String introduce;
+	private String userAddress;
+	private int totalServiceCnt;
+	private int totalServiceScore;
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getIntroduce() {
+		return introduce;
+	}
+
+	public void setIntroduce(String introduce) {
+		this.introduce = introduce;
+	}
+
+	public String getUserAddress() {
+		return userAddress;
+	}
+
+	public void setUserAddress(String userAddress) {
+		this.userAddress = userAddress;
+	}
+	
+	public int getTotalServiceCnt() {
+		return totalServiceCnt;
+	}
+	
+	public void setTotalServiceCnt(int totalServiceCnt) {
+		this.totalServiceCnt = totalServiceCnt;
+	}
+	
+	public int getTotalServiceScore() {
+		return totalServiceScore;
+	}
+	
+	public void setTotalServiceScore(int totalServiceScore) {
+		this.totalServiceScore = totalServiceScore;
+	}
+}
